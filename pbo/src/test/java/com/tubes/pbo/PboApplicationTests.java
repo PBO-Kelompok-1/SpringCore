748ebe7549a1bd3d1c614923d27d657e1ded9763
Bagus Krishna <bagus.krisna2802@gmail.com>
2024-12-18 19:49:53 +0700

    first setup

---
+++ pbo/src/test/java/com/tubes/pbo/PboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubes.pbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
