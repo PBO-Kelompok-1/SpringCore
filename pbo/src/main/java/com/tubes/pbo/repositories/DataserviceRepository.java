ef09c3807ee26038de5bda1910f58ad5eb9c9730
Azmi1180 <muhammadazmi4456@gmail.com>
2024-12-19 12:04:29 +0700

    Hapus DataserviceRepository

--- pbo/src/main/java/com/tubes/pbo/repositories/DataserviceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tubes.pbo.repositories;
-
-import com.tubes.pbo.models.dataservice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DataserviceRepository extends JpaRepository<dataservice, Integer> {
-}
